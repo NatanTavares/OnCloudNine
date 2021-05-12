f965876de314285e3be4a8c8bdc38a363fc63364
NatanTavares <nattantavares.s15@gmail.com>
2021-05-12 13:52:24 -0300

    refactor(component)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,54 +1,51 @@
-import { useState } from 'react';
-import ReactSwitch from 'react-switch'
+import { useState } from "react";
+import ReactSwitch from "react-switch";
 
 import {
   IoSyncOutline,
-  
   IoSunnyOutline,
   IoPartlySunnyOutline,
   IoMoonOutline,
   IoCloudyNightOutline,
-
   IoCloudOutline,
   IoRainyOutline,
   IoThunderstormOutline,
-  IoSnowOutline
-} from 'react-icons/io5'
+  IoSnowOutline,
+} from "react-icons/io5";
 
-import styles from '../styles/components/Header.module.css'
+import styles from "../styles/components/Header.module.css";
 
 export function Header() {
   const [inImperialMeasure, setInImperialMeasure] = useState(true);
-  const [isMusicOn, setIsMusicOn] = useState(true);
 
   function renderTheCurrentWeatherIcon(option: number) {
     switch (option) {
       case 0:
-        return <IoSunnyOutline title="Sunny" size={30} />
-      
-      case 1:
-        return <IoPartlySunnyOutline title="Partly sunny" size={30} />
-      
-      case 2: 
-       return <IoMoonOutline title="Moon" size={30} />
+        return <IoSunnyOutline title="Sunny" size={30} />;
 
-      case 3: 
-        return <IoCloudyNightOutline title="Cloudy night" size={30} />
+      case 1:
+        return <IoPartlySunnyOutline title="Partly sunny" size={30} />;
+
+      case 2:
+        return <IoMoonOutline title="Moon" size={30} />;
+
+      case 3:
+        return <IoCloudyNightOutline title="Cloudy night" size={30} />;
 
       case 4:
-        return <IoCloudOutline title="Cloud" size={30} />
-      
+        return <IoCloudOutline title="Cloud" size={30} />;
+
       case 5:
-        return <IoRainyOutline title="Rainy" size={30} />
-      
+        return <IoRainyOutline title="Rainy" size={30} />;
+
       case 6:
-        return <IoThunderstormOutline title="Thunderstorm" size={30} />
-      
+        return <IoThunderstormOutline title="Thunderstorm" size={30} />;
+
       case 7:
-        return <IoSnowOutline title="Snow" size={30} />
-            
+        return <IoSnowOutline title="Snow" size={30} />;
+
       default:
-        console.error('>[renderTheCurrentWeatherIcon]: Icon found out!');
+        console.error(">[renderTheCurrentWeatherIcon]: Icon found out!");
         return null;
     }
   }
@@ -76,50 +73,26 @@ export function Header() {
         </section>
 
         <div className={styles.switchesContainer}>
-          <div>
-            <span>°F / °C</span>
-            <div className={styles.switch}>
-              <ReactSwitch 
-                onChange={event => setInImperialMeasure(!inImperialMeasure)} 
-                checked={inImperialMeasure}
-                
-                height={14}
-                width={24}
-                handleDiameter={14}
-                borderRadius={10}
+          <p>
+            <span className={!inImperialMeasure && styles.enabled}>°F</span>/
+            <span className={!!inImperialMeasure && styles.enabled}>°C</span>
+          </p>
 
-                onHandleColor={'#a3ffe3'}
-                offHandleColor={'#a3ffe3'}
-                checkedIcon={false}
-                uncheckedIcon={false}
-
-                onColor={'#8D9BBA'}
-                offColor={'#8D9BBA'}
-              />
-            </div>
-          </div>
-
-          <div>
-            <span>Music</span>
-            <div className={styles.switch}>
-              <ReactSwitch 
-                onChange={event => setIsMusicOn(!isMusicOn)} 
-                checked={isMusicOn}
-                
-                height={14}
-                width={24}
-                handleDiameter={14}
-                borderRadius={10}
-
-                onColor={'#8D9BBA'}
-                offColor={'#8D9BBA'}
-                onHandleColor={'#a3ffe3'}
-                offHandleColor={'#ffffff'}
-
-                checkedIcon={false}
-                uncheckedIcon={false}
-              />
-            </div>
+          <div className={styles.switch}>
+            <ReactSwitch
+              checked={inImperialMeasure}
+              onChange={() => setInImperialMeasure(!inImperialMeasure)}
+              height={14}
+              width={24}
+              handleDiameter={14}
+              borderRadius={10}
+              onHandleColor={"#ffffff"}
+              offHandleColor={"#ffffff"}
+              checkedIcon={false}
+              uncheckedIcon={false}
+              onColor={"#8D9BBA"}
+              offColor={"#8D9BBA"}
+            />
           </div>
         </div>
       </header>
