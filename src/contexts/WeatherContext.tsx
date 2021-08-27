c0019198efffcee60ce437fc22aacc257cf493a4
NatanTavares <nattantavares.s15@gmail.com>
2021-08-27 11:45:11 -0300

    feat: Defines icons conditionally

--- src/contexts/WeatherContext.tsx
+++ src/contexts/WeatherContext.tsx
@@ -1,25 +1,42 @@
 import { createContext, ReactNode, useState } from "react";
 import { useError } from "../hooks/useError";
+import { IconType } from "react-icons/lib";
 import axios from "axios";
+
+import {
+  WiCloud,
+  WiDayCloudy,
+  WiDaySunny,
+  WiFog,
+  WiHail,
+  WiNa,
+  WiNightAltCloudy,
+  WiNightClear,
+  WiRain,
+  WiSnow,
+  WiThunderstorm,
+} from "react-icons/wi";
 
 type ResponseData = {
   data: {
     results: {
-      temp: number;
-      date: string;
       city: string;
+      date: string;
+      condition_slug: string;
+      temp: number;
     };
   };
 };
 
 type WeatherData = {
-  temp: number;
-  date: string;
   city: string;
+  date: string;
+  icon: IconType;
+  temp: number;
 };
 
 type ContextData = {
-  data: WeatherData;
+  data: WeatherData | null;
   loading: boolean;
   getWeatherData: (cityName: string) => Promise<void>;
 };
@@ -31,10 +48,39 @@ type ProviderProps = {
 export const WeatherDataContext = createContext({} as ContextData);
 
 export function WeatherDataProvider({ children }: ProviderProps) {
-  const { notifyErr } = useError();
-
   const [data, setData] = useState<WeatherData | null>(null);
   const [loading, setLoading] = useState(false);
+  const { notifyErr } = useError();
+
+  function setCurrentlyIcon(condition_slug: string) {
+    switch (condition_slug) {
+      case "clear_day":
+        return WiDaySunny;
+      case "cloudly_day":
+        return WiDayCloudy;
+
+      case "clear_night":
+        return WiNightClear;
+      case "cloudly_night":
+        return WiNightAltCloudy;
+
+      case "storm":
+        return WiThunderstorm;
+      case "snow":
+        return WiSnow;
+      case "hail":
+        return WiHail;
+      case "rain":
+        return WiRain;
+      case "fog":
+        return WiFog;
+      case "cloud":
+        return WiCloud;
+
+      default:
+        return WiNa;
+    }
+  }
 
   async function getWeatherData(cityName: string) {
     setLoading(true);
@@ -42,9 +88,10 @@ export function WeatherDataProvider({ children }: ProviderProps) {
       const response = await axios.post<ResponseData>("api/weatherData", {
         cityName,
       });
-      const { city, date, temp } = response.data.data.results;
-      setData({ city, date, temp });
+      const { city, condition_slug, date, temp } = response.data.data.results;
+      const icon = setCurrentlyIcon(condition_slug);
 
+      setData({ city, date, icon, temp });
       setLoading(false);
     } catch (error) {
       notifyErr({ err: error });
