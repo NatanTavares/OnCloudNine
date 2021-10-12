2634061448d756a826058be0e81012521e5b22d4
NatanTavares <nattantavares.s15@gmail.com>
2021-10-12 11:54:38 -0300

    refactor: Implements verification of the requested parameters

--- src/pages/api/weatherData.ts
+++ src/pages/api/weatherData.ts
@@ -5,10 +5,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     try {
       const cityName: string = req.body.cityName;
-      const { data } = await api.get(
-        `weather?key=${process.env.HGBRASIL_KEY}&city_name=${cityName}`
-      );
-      return res.json({ data });
+      if (!!cityName) {
+        const { data } = await api.get(
+          `weather?key=${process.env.HGBRASIL_KEY}&city_name=${cityName}`
+        );
+        return res.json({ data });
+      }
+
+      return res
+        .status(400)
+        .end(
+          "The request failed because cityname is required for this operation"
+        );
     } catch (error) {
       return res.json({ error: "Failed request for weather data" });
     }
